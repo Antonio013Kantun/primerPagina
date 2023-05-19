0d9a21566f1d9bb8d0c828578febbd6948c1fe90
Antonio013Kantun <tony13.aekc@gmail.com>
2023-05-18 18:45:10 -0600

    Primer commit

---
+++ css/action.js
@@ -0,0 +1,24 @@
+// const optionBtns = document.querySelectorAll('.option-btn');
+
+// optionBtns.forEach((btn) => {
+//   btn.addEventListener('click', () => {
+//     const target = btn.dataset.target;
+//     const options = document.querySelectorAll('.option');
+
+//     options.forEach((option) => {
+//       option.classList.remove('active');
+//     });
+
+//     const targetOption = document.querySelector(`#${target}`);
+//     targetOption.classList.add('active');
+
+//     optionBtns.forEach((btn) => {
+//       btn.classList.remove('active');
+//     });
+
+//     btn.classList.add('active');
+//   });
+// });
+
+//ctrl C A = SELECCIONAR TODO EL CONTENIDO
+//ctrl K C = COMENTAR EL CONTENIDO
